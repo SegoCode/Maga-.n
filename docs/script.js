f10fb062f128a135c766c3cf50b07e5e2c3ebd1c
SegoCode <35817798+SegoCode@users.noreply.github.com>
2023-03-29 11:40:39 +0200

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,103 +1,85 @@
 console.log('%c👋', 'color: #36393e; font-size: 30px;');
 
-var firstLoad =true;
-var loadedItems = 0;
-var realLoaded = 0;
+let firstLoad = true;
+let loadedItems = 0;
+let realLoaded = 0;
+
 async function loadList() {
-  let url = 'https://raw.githubusercontent.com/SegoCode/Maga-.n/main/database/db.json';
-  let objData = await (await fetch(url)).json();
+    const url = 'https://raw.githubusercontent.com/SegoCode/Maga-.n/main/database/db.json';
+    const objData = await (await fetch(url)).json();
+    let loadedItemsGoal = loadedItems + 50;
+    const searchParams = new URLSearchParams(window.location.search);
+    const genderParam = searchParams.get("gender");
 
-  let loadedItemsGoal = loadedItems + 50;
-  const searchParams = new URLSearchParams(window.location.search);
-  const genderParam = searchParams.get("gender");
+    for (let i = loadedItems; i < loadedItemsGoal; i++) {
+        let addCard = false;
 
-  for (var i = loadedItems; i < loadedItemsGoal; i++) {
-    let addCard = false;
-  
-    if (genderParam) {
-      if (objData[i].hasOwnProperty("genres")) {
-        for (const genre of objData[i].genres) {
-           if(genre.name===genderParam){
-              addCard = true;
-           }
+        if (genderParam && objData[i].hasOwnProperty("genres")) {
+            addCard = objData[i].genres.some(genre => genre.name === genderParam);
+        } else {
+            addCard = true;
         }
-      }
-    } else {
-      addCard = true;
-    }
-  
-    if (addCard) {
-      try {
-        document.getElementById('magaList').innerHTML += `
-          <div class="card-container">
-            <div class="card" onclick="window.open('${objData[i].url_listado_manga}', '_blank');">
-              <div class="card-content" style="background-image: url('${objData[i].images.webp.image_url}');"></div>
-            </div>
-            <div>${objData[i].title_spanish}</div>
-          </div>
-        `;
-        realLoaded++;
-      } catch (error) {
-        // Fix url_listado_manga undefine para mangas no listados
-      }
-    }
- 
-  }
 
-  loadedItems = loadedItems + 50;
-  window.addEventListener("scroll", handleInfiniteScroll);
-  if(loadedItems<objData.length){
-    if(realLoaded<50){
-      loadList();
-    }
-  }
-
- 
-  if (firstLoad) {
-  const genres = new Set(); // Create a Set to store the unique genres
-  // Loop through the JSON array and add each genre to the Set, if the manga has genres
-  for (const manga of objData) {
-    if (manga.hasOwnProperty("genres")) {
-      for (const genre of manga.genres) {
-        genres.add(genre.name);
-      }
-    }
-  }
-  const mangaList = document.getElementById("mangaGender"); // Get the mangaList div from the HTML document
-  // Loop through the unique genres and create a button for each one
-    for (const genre of genres) {
-      const button = document.createElement("button"); // Create a new button element
-      button.classList.add("tag"); // Add the "tag" class to the button
-      button.textContent = genre; // Set the text content of the button to the genre name
-
-      button.addEventListener("click", () => {
-        // Get the current URL
-        const currentUrl = new URL(window.location.href);
-        // Set the "gender" parameter to the genre name
-        currentUrl.searchParams.set("gender", genre);
-        // Update the URL with the new search parameters
-        window.history.replaceState({}, "", currentUrl.toString());
-        // Reload the page
-        location.reload();
-      });
-      mangaList.appendChild(button); // Append the button to the mangaList div
+        if (addCard) {
+            try {
+                document.getElementById('magaList').innerHTML += `
+                    <div class="card-container">
+                        <div class="card" onclick="window.open('${objData[i].url_listado_manga}', '_blank');">
+                            <div class="card-content" style="background-image: url('${objData[i].images.webp.image_url}');"></div>
+                        </div>
+                        <div>${objData[i].title_spanish}</div>
+                    </div>
+                `;
+                realLoaded++;
+            } catch (error) {
+                // Fix url_listado_manga undefined for unlisted mangas
+            }
+        }
     }
 
-    firstLoad=false;
-  }
+    loadedItems += 50;
+    window.addEventListener("scroll", handleInfiniteScroll);
 
+    if (loadedItems < objData.length && realLoaded < 50) {
+        loadList();
+    }
 
+    if (firstLoad) {
+        const genres = new Set();
+        objData.forEach(manga => {
+            if (manga.hasOwnProperty("genres")) {
+                manga.genres.forEach(genre => genres.add(genre.name));
+            }
+        });
+
+        const mangaList = document.getElementById("mangaGender");
+        genres.forEach(genre => {
+            const button = document.createElement("button");
+            button.classList.add("tag");
+            button.textContent = genre;
+
+            button.addEventListener("click", () => {
+                const currentUrl = new URL(window.location.href);
+                currentUrl.searchParams.set("gender", genre);
+                window.history.replaceState({}, "", currentUrl.toString());
+                location.reload();
+            });
+
+            mangaList.appendChild(button);
+        });
+
+        firstLoad = false;
+    }
 }
 
 const handleInfiniteScroll = () => {
-  if ((window.innerHeight + window.pageYOffset) >= (document.body.offsetHeight - 500)) {
-    loadList();
-    window.removeEventListener("scroll", handleInfiniteScroll);
-    realLoaded=0;
-  }
+    if ((window.innerHeight + window.pageYOffset) >= (document.body.offsetHeight - 500)) {
+        loadList();
+        window.removeEventListener("scroll", handleInfiniteScroll);
+        realLoaded = 0;
+    }
 }
 
-//init
+// Initialize
 loadList();
 window.addEventListener("scroll", handleInfiniteScroll);
-
